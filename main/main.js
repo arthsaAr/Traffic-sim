2fa3273e98c1e6b1e3bb6693c38ed0e9cf97f0fe
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-07 22:10:36 -0700

    re-organised the overall structure
    refined the canvas and added road like design

--- main/main.js
+++ main/main.js
@@ -3,8 +3,29 @@ const contents = canvas.getContext("2d");
 
 function drawIntersection() {
     contents.fillStyle = "#333";
-    contents.fillRect(0,350,800,100);       //horizontal road
-    contents.fillRect(350,0,100,800);       //verticle road
+    contents.fillRect(0,325,800,150);       //horizontal road
+    contents.fillRect(325,0,150,800);       //verticle road
+
+    //dashed yellow lines
+    contents.strokeStyle = "yellow";
+    contents.lineWidth = 4;
+    contents.setLineDash([20,15]); //this makes 20px dash with 15px gap
+
+    //first verticle line
+    contents.beginPath();
+    contents.moveTo(400,0);
+    contents.lineTo(400, 800);
+    contents.stroke();
+
+    //horizontal line
+    contents.beginPath()
+    contents.moveTo(0,400);
+    contents.lineTo(800, 400);
+    contents.stroke();
+
+    //this is for reseting the dash
+    contents.setLineDash([]);
+
 }
 
 drawIntersection();
