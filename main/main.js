d3683f0ece0eb455e9d717d0d27cbb16639b5ae7
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-07 22:39:24 -0700

    made white traffic light section for all four roads in intersection

--- main/main.js
+++ main/main.js
@@ -26,6 +26,36 @@ function drawIntersection() {
     //this is for reseting the dash
     contents.setLineDash([]);
 
+
+    //traffic line drawing (right road)
+    const boxWidth1 = 15;
+    const boxHeight1 = 70;
+    const posX1 = 483 - boxWidth1/2;
+    const posY1 = 440 +  - boxHeight1/2;
+
+    contents.fillStyle="white";
+    contents.fillRect(posX1, posY1, boxWidth1, boxHeight1);
+
+    //left road
+    const posX2 = 318 - boxWidth1/2;
+    const posY2 = 360 +  - boxHeight1/2;
+    contents.fillStyle="white";
+    contents.fillRect(posX2, posY2, boxWidth1, boxHeight1);
+
+    //top road
+    const boxWidth2 = 70;
+    const boxHeight2 = 15;
+    const posX3 = 440 - boxWidth2/2;
+    const posY3 = 315 +  - boxHeight2/2;
+    contents.fillStyle= "white";
+    contents.fillRect(posX3, posY3, boxWidth2, boxHeight2);
+
+    //bottom road
+    const posX4 = 360 - boxWidth2/2;
+    const posY4 = 483 +  - boxHeight2/2;
+    contents.fillStyle= "white";
+    contents.fillRect(posX4, posY4, boxWidth2, boxHeight2);
 }
+
 
 drawIntersection();
