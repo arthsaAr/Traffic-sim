f2a6dd57d2ac4e0609baab6f6497490373cf39f9
arthsaAr <kcraashtra431qwe@gmail.com>
2026-02-08 21:44:14 -0700

    implemented a traffic light changing logic
    still needs work

--- main/main.js
+++ main/main.js
@@ -1,6 +1,46 @@
 const canvas = document.getElementById("simCanvas");
 const contents = canvas.getContext("2d");
 
+let currentLight_verticle = "green";
+let currentLight_horizontal = "red";
+let lastSwitchTime_verticle = Date.now();
+let lastSwitchTime_horizontal = Date.now();
+
+const lightsDurations = {
+    green: 30000,       //30 seconds
+    orange: 5000,       //5 seconds
+    red: 20000      //20 seconds
+}
+
+function updateTrafficLight() {
+    const now = Date.now();
+    const elapsed1 = now -lastSwitchTime_horizontal;
+
+    if(elapsed1 >= lightsDurations[currentLight_horizontal]){
+        if(currentLight_horizontal === "green"){
+            currentLight_horizontal = "orange";
+        } else if(currentLight_horizontal === "orange"){
+            currentLight_horizontal = "red";
+        }else {
+            currentLight_horizontal = "green";
+        }
+        lastSwitchTime_horizontal = now;
+    }
+
+    const elapsed2 = now -lastSwitchTime_verticle;
+
+    if(elapsed2 >= lightsDurations[lastSwitchTime_verticle]){
+        if(lastSwitchTime_verticle === "red"){
+            lastSwitchTime_verticle = "green";
+        } else if(lastSwitchTime_verticle === "green"){
+            lastSwitchTime_verticle = "orange";
+        }else {
+            lastSwitchTime_verticle = "red";
+        }
+        lastSwitchTime_verticle = now;
+    }
+}
+
 function drawIntersection() {
     contents.fillStyle = "#333";
     contents.fillRect(0,325,800,150);       //horizontal road
@@ -34,14 +74,14 @@ function drawIntersection() {
     const posY1 = 440 +  - boxHeight1/2;
 
     contents.fillStyle="white";
-    drawTrafficBox(483 - boxWidth1/2, 440 - boxHeight1/2, boxWidth1, boxHeight1, true, "red");
+    drawTrafficBox(483 - boxWidth1/2, 440 - boxHeight1/2, boxWidth1, boxHeight1, true, currentLight_horizontal);
     // contents.fillRect(posX1, posY1, boxWidth1, boxHeight1);
 
     //left road
     const posX2 = 318 - boxWidth1/2;
     const posY2 = 360 +  - boxHeight1/2;
     contents.fillStyle="white";
-    drawTrafficBox(318 - boxWidth1/2, 360 - boxHeight1/2, boxWidth1, boxHeight1, true, "orange");
+    drawTrafficBox(318 - boxWidth1/2, 360 - boxHeight1/2, boxWidth1, boxHeight1, true, currentLight_horizontal);
     // contents.fillRect(posX2, posY2, boxWidth1, boxHeight1);
 
     //top road
@@ -50,14 +90,14 @@ function drawIntersection() {
     const posX3 = 440 - boxWidth2/2;
     const posY3 = 315 +  - boxHeight2/2;
     contents.fillStyle= "white";
-    drawTrafficBox(440 - boxWidth2/2, 315 - boxHeight2/2, boxWidth2, boxHeight2, false , "red");
+    drawTrafficBox(440 - boxWidth2/2, 315 - boxHeight2/2, boxWidth2, boxHeight2, false , currentLight_verticle);
     // contents.fillRect(posX3, posY3, boxWidth2, boxHeight2);
 
     //bottom road
     const posX4 = 360 - boxWidth2/2;
     const posY4 = 483 +  - boxHeight2/2;
     contents.fillStyle= "white";
-    drawTrafficBox(360 - boxWidth2/2, 483 - boxHeight2/2, boxWidth2, boxHeight2, false, "green");
+    drawTrafficBox(360 - boxWidth2/2, 483 - boxHeight2/2, boxWidth2, boxHeight2, false, currentLight_verticle);
     // contents.fillRect(posX4, posY4, boxWidth2, boxHeight2);
 }
 
@@ -106,4 +146,14 @@ function drawTrafficBox(x,y,width, height, isVertical = true, active = null){
     }
 }
 
-drawIntersection();
+function loop(){
+    contents.clearRect(0, 0, canvas.width, canvas.height);
+
+    updateTrafficLight();
+    drawIntersection();
+
+    requestAnimationFrame(loop);
+}
+
+// drawIntersection();
+loop();
